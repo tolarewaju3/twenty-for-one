4e8afd3bb2ddfa7a1b42bacdc6c948793828a842
Tyler Russell <trussell2671@gmail.com>
2020-04-03 17:11:43 -0400

    Update nodejs

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const express = require('express');
+const {Datastore} = require('@google-cloud/datastore');
+
+// Creates a client
+const datastore = new Datastore();
 
 // Constants
 const PORT = 8080;
@@ -9,8 +13,33 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
+  quickstart();
   res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+async function quickstart() {
+  // The kind for the new entity
+  const kind = 'Task';
+
+  // The name/ID for the new entity
+  const name = 'sampletask1';
+
+  // The Cloud Datastore key for the new entity
+  const taskKey = datastore.key([kind, name]);
+
+  // Prepares the new entity
+  const task = {
+    key: taskKey,
+    data: {
+      description: 'Buy milk',
+    },
+  };
+
+  // Saves the entity
+  await datastore.save(task);
+  console.log(`Saved ${task.key.name}: ${task.data.description}`);
+}
+
