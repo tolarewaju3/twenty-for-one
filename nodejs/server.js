f16f526b22950e1feb52d66dcdc867ae990dd001
Tyler Russell <trussell2671@gmail.com>
2020-04-03 18:40:06 -0400

    Update server and deploy

--- nodejs/server.js
+++ nodejs/server.js
@@ -10,36 +10,26 @@ const datastore = new Datastore();
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+var output = "";
+const query = datastore.createQuery('Delivery');
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  quickstart();
-  res.send('Hello World');
+  queryDb(res)
 });
+
+async function queryDb(response) {
+  const [deliveries] = await datastore.runQuery(query);
+  deliveries.forEach(delivery => {
+    output = output + delivery['helperName'] + " delivered for " + delivery['olderAdult']
+  });
+
+  response.send(output);
+}
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
-async function quickstart() {
-  // The kind for the new entity
-  const kind = 'Task';
 
-  // The name/ID for the new entity
-  const name = 'sampletask1';
-
-  // The Cloud Datastore key for the new entity
-  const taskKey = datastore.key([kind, name]);
-
-  // Prepares the new entity
-  const task = {
-    key: taskKey,
-    data: {
-      description: 'Buy milk',
-    },
-  };
-
-  // Saves the entity
-  await datastore.save(task);
-  console.log(`Saved ${task.key.name}: ${task.data.description}`);
-}
 
